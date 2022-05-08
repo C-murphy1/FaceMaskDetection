e8d589b8a0722ee09ee2f33099152ca72c852c8b
Cameron <cmurphy5@stevens.edu>
2022-05-08 17:04:57 -0400

    Updated Model training

--- MaskDetectionTrain.py
+++ MaskDetectionTrain.py
@@ -4,19 +4,24 @@ import PIL
 import tensorflow as tf
 
 dataset_dir = "traindata"
+testdata_dir = "testdata"
 
-batch_size = 4
-img_height = 180
-img_width = 180
+batch_size = 1
+img_height = 280
+img_width = 280
 
 data = tf.keras.utils.image_dataset_from_directory(
   dataset_dir,
   color_mode='grayscale',
-  validation_split=0.4,
+  validation_split=0.2,
   subset="training",
   seed=123,
   image_size=(img_height, img_width),
   batch_size=batch_size)
+
+
+tensorboard = tf.keras.callbacks.TensorBoard(log_dir="logs")
+
 
 
 optimizer = tf.keras.optimizers.Adagrad(0.01)
@@ -31,6 +36,7 @@ model.add(tf.keras.layers.Conv2D(64, 3, padding='same', activation='relu'))
 model.add(tf.keras.layers.MaxPooling2D())
 model.add(tf.keras.layers.Flatten())
 model.add(tf.keras.layers.Dense(128,activation='relu'))
+model.add(tf.keras.layers.Dense(128,activation='relu'))
 
 model.add(tf.keras.layers.Dense(2,activation='softmax'))
 
@@ -38,6 +44,15 @@ model.compile(optimizer=optimizer,
               loss=tf.keras.losses.SparseCategoricalCrossentropy(),
                 metrics=['accuracy'])
 
-model.fit(data,epochs=5,batch_size=4)
+model.fit(data,epochs=5,batch_size=1,callbacks=[tensorboard])
+
+data = tf.keras.utils.image_dataset_from_directory( #Loads in separate training data to test the model
+  testdata_dir,
+  color_mode='grayscale',
+  seed=123,
+  image_size=(img_height, img_width),
+  batch_size=batch_size)
+
+model.evaluate(data, batch_size=1)
 
 model.save('model')
