af7a1faff62ad9c82df2e735438bb1451db04de3
Luisa Bonfim <78322824+luisalacet@users.noreply.github.com>
2022-05-08 20:48:18 -0400

    considering validation slit

--- MaskDetectionTrain.py
+++ MaskDetectionTrain.py
@@ -1,27 +1,38 @@
-import numpy as np
 import os
 import PIL
 import tensorflow as tf
 
-dataset_dir = "traindata"
+traindata_dir = "traindata"
 testdata_dir = "testdata"
+validationdata_dir = "validationdata"
 
 batch_size = 1
 img_height = 280
 img_width = 280
 
-data = tf.keras.utils.image_dataset_from_directory(
-  dataset_dir,
+traindata = tf.keras.utils.image_dataset_from_directory(
+  traindata_dir,
   color_mode='grayscale',
-  validation_split=0.2,
-  subset="training",
+  seed=123,
+  image_size=(img_height, img_width),
+  batch_size=batch_size)
+
+validationdata = tf.keras.utils.image_dataset_from_directory(
+  validationdata_dir,
+  color_mode='grayscale',
+  seed=123,
+  image_size=(img_height, img_width),
+  batch_size=batch_size)
+
+testdata = tf.keras.utils.image_dataset_from_directory( #Loads in separate training data to test the model
+  testdata_dir,
+  color_mode='grayscale',
   seed=123,
   image_size=(img_height, img_width),
   batch_size=batch_size)
 
 
 tensorboard = tf.keras.callbacks.TensorBoard(log_dir="logs")
-
 
 
 optimizer = tf.keras.optimizers.Adagrad(0.01)
@@ -44,15 +55,8 @@ model.compile(optimizer=optimizer,
               loss=tf.keras.losses.SparseCategoricalCrossentropy(),
                 metrics=['accuracy'])
 
-model.fit(data,epochs=5,batch_size=1,callbacks=[tensorboard])
+model.fit(traindata,epochs=5,batch_size=1,callbacks=[tensorboard],validation_data=validationdata)
 
-data = tf.keras.utils.image_dataset_from_directory( #Loads in separate training data to test the model
-  testdata_dir,
-  color_mode='grayscale',
-  seed=123,
-  image_size=(img_height, img_width),
-  batch_size=batch_size)
-
-model.evaluate(data, batch_size=1)
+model.evaluate(testdata, batch_size=1)
 
 model.save('model')
